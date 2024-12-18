1b2840c73ce7b1afe0cd265d0333854af003eec0
AlexandrVino <alexander-vin0@yandex.ru>
2024-12-18 13:18:20 +0500

    Fix files-io/read-filter/write

--- files-io/read-filter-write/solution.c
+++ files-io/read-filter-write/solution.c
@@ -3,6 +3,9 @@
 #include <stdint.h>
 #include <fcntl.h>
 
+
+const int BUFFER_SIZE = 4096;
+
 struct Item {
   int value;
   uint32_t next_pointer;
@@ -20,7 +23,6 @@ int close_all(int fd1, int fd2, int fd3) {
 
 int main(int argc, char const* argv[])
 {    
-    char item;
     int fd = open(argv[1], O_RDONLY);
     if (fd == -1) {    
         return 1;
@@ -37,29 +39,55 @@ int main(int argc, char const* argv[])
         return 2;
     }
 
+    char buffer[BUFFER_SIZE];
+    char bufferNums[BUFFER_SIZE];
+    char bufferOthers[BUFFER_SIZE];
+
+    int buffNumIndex = 0;
+    int buffOtherIndex = 0;
+
     while (1) {
-        int res = read(fd, &item, sizeof(char));
+        int res = read(fd, &buffer, sizeof(buffer));
         if (res == -1) {
             close_all(fd, fd_int_out, fd_other_out);
             return 3;
         }
         if (res == 0) break;
 
-        if ('0' <= item && '9' >= item) {
-            // printf("%c", item);
-            if (write(fd_int_out, &item, sizeof(char)) == -1) {
-                close_all(fd, fd_int_out, fd_other_out);
-                return 3;
+        for (int i = 0; i < res / sizeof(char); ++i) {
+            if ('0' <= buffer[i] && '9' >= buffer[i]) {
+                bufferNums[buffNumIndex++] = buffer[i];
+                if (buffNumIndex >= BUFFER_SIZE) {
+                    buffNumIndex = 0;
+                    if (write(fd_int_out, &bufferNums, sizeof(bufferNums)) == -1) {
+                        close_all(fd, fd_int_out, fd_other_out);
+                        return 3;
+                    }
+                }
+            } else {
+                bufferOthers[buffOtherIndex++] = buffer[i];
+                if (buffOtherIndex >= BUFFER_SIZE) {
+                    buffOtherIndex = 0;
+                    if (write(fd_other_out, &bufferOthers, sizeof(bufferOthers)) == -1) {
+                        close_all(fd, fd_int_out, fd_other_out);
+                        return 3;
+                    }
+                }
             }
-        } else {
-            if (write(fd_other_out, &item, sizeof(char)) == -1) {
+        }
+
+        if (buffNumIndex > 0) {
+            if (write(fd_int_out, &bufferNums, sizeof(char) * buffNumIndex) == -1) {
                 close_all(fd, fd_int_out, fd_other_out);
                 return 3;
             }
         }
-         
-        if (item == '\0') {
-            break;
+
+        if (buffOtherIndex > 0) {
+            if (write(fd_other_out, &bufferOthers, sizeof(char) * buffOtherIndex) == -1) {
+                close_all(fd, fd_int_out, fd_other_out);
+                return 3;
+            }
         }
     }
     
--- texts/normalize-path/solution.c
+++ texts/normalize-path/solution.c
@@ -4,6 +4,51 @@
 extern void normalize_path(char *path) {
     fgets(path, sizeof(path), stdin);
     
-    strcspn(path, "\n");
+    int i = 0;
+    while (*(path + i) != '\0') {
+        if (*(path + i) == ' ') continue;
+        printf("%c.", *(path + i));
+        ++i;
+    }
+    printf("\n");
+
+    i = 0;
+    while (*(path + i) != '\0') {
+        if (*(path + i) == '.') {
+            if (*(path + i + 1) == '.') {
+                *(path + i - 1) = ' ';
+                *(path + i) = ' ';
+                *(path + i + 1) = ' ';
+                *(path + i + 2) = ' ';
+                int j = i - 1;
+                while (j > 0 && *(path + j - 1) != '/') {
+                    *(path + j - 1) = ' ';
+                    --j;
+                }
+                ++i;
+                ++i;
+                ++i;                
+            } else if (*(path + i + 1) == '/') {
+                *(path + i) = ' ';
+                *(path + i + 1) = ' ';
+                ++i;
+                ++i;
+            }
+        } else if (*(path + i) == '/' && *(path + i + 1) == '/') {
+            *(path + i) = ' ';
+            ++i;
+        } else {
+            ++i;
+        }
+    }
+    
+    i = 0;
+    while (*(path + i) != '\0') {
+        if (*(path + i) == ' ') continue;
+        printf("%c", *(path + i));
+        ++i;
+    }
+    printf("\n");
+    
     return 0;
 }
