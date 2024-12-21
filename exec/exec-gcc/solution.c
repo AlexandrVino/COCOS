19e8bac2f3545a9a2e1bf4d2181257c27c1eea49
AlexandrVino <alexander-vin0@yandex.ru>
2024-12-21 14:03:21 +0500

    Add exec

--- exec/exec-gcc/solution.c
+++ exec/exec-gcc/solution.c
@@ -1,35 +1,49 @@
 #include <unistd.h>
 
+int is_empty(char s)
+{
+  return s == ' ' || s == '\t' || s == '\n';
+}
+
 const int BUFFER_SIZE = 4096;
 
 const char *TMP =
-    "gcc -o out.out -x c - #include <stdio.h>\\nint main() { printf(\"%%d\\n\", %%s); return 0; }\\neof";
+    "gcc -o out.out -x c - <<eof\n#include <stdio.h>\nvoid main() { "
+    "printf(\"%%d\\n\", (%s)); }\neof";
 
-int *trim(char *s)
+char *trim(char *s)
 {
+  int len = 0;
   char *start = s;
-  while (isspace(*s))
+  while (is_empty(*s))
     s++;
-  return s - start;
+
+  char *end = s + strlen(s) - 1;
+  while (end > s && *end != '\n')
+  {
+    *end-- = '\0';
+  }
+  *end = '\0';
+  return s;
 }
 
 int main(int argc, char const *argv[])
 {
-  char exp_buffer[BUFFER_SIZE];
-  int amount = read(STDIN_FILENO, exp_buffer, BUFFER_SIZE);
+  char buffer[BUFFER_SIZE];
+  int amount = read(0, buffer, BUFFER_SIZE);
   if (amount == -1)
   {
-    exit(1);
+    return 0;
   }
 
-  if (trim(exp_buffer) == 0)
+  char *string = trim(buffer);
+  if (*string == '\0')
   {
     return 0;
   }
 
   char template_buffer[BUFFER_SIZE];
-  snprintf(template_buffer, BUFFER_SIZE, TMP, exp_buffer);
-  printf("%s\n", template_buffer);
+  snprintf(template_buffer, BUFFER_SIZE, TMP, string);
   system(template_buffer);
 
   execl("./out.out", (char *)NULL);
