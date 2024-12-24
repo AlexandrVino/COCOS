8eeb94e65418e771244e3da0982f81e7d046bdc5
AlexandrVino <alexander-vin0@yandex.ru>
2024-12-24 14:09:40 +0500

    Add asm syscall && edit forks

--- fork/proc-print-numbers/solution.c
+++ fork/proc-print-numbers/solution.c
@@ -4,37 +4,34 @@
 #include <unistd.h>
 
 const int BUFFER_SIZE = 4096;
+int index = 1;
 
-void print_via_fork(int curr, int n)
+void print_via_fork(int n)
 {
-    if (curr <= 0) 
-        exit(0);
-    
-    pid_t pid = fork();
-    if (pid == -1)
+    for (int i = 1; i <= n; ++i)
     {
-        exit(1);
-    }
+        pid_t pid = fork();
+        if (pid == -1)
+            exit(1);
 
-    if (pid == 0)
-    {
-        print_via_fork(curr - 1, n);
-        exit(0);
-    }
-    else
-    {
+        if (pid == 0)
+        {
+            printf("%d", i);
+            printf(i == n ? "\n" : " ");
+            exit(0);
+        }
         wait(NULL);
-        printf("%d", curr);
-        printf(curr == n ? "\n": " ");
     }
 }
 
 int main(int argc, char const *argv[])
 {
-    if (argc < 2) {
+    if (argc < 2)
+    {
         exit(1);
     }
     int n;
     sscanf(argv[1], "%d", &n);
-    print_via_fork(n, n);
+
+    print_via_fork(n);
 }
